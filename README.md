be7b69b9dca031828319c1976eb70d99c911a3a4
techbrunomed <techbrunomed@gmail.com>
2025-01-04 13:32:45 +0000

    adiciona scripts `lint:check`e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # primeiro-repositorio
+
 aprendendo programação
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-    return <h1>Cada momento ao seu lado é um presente. Te amo meu Amor "Renata Zappelini"</h1>
+  return (
+    <h1>
+      Cada momento ao seu lado é um presente. Te amo meu Amor "Renata Zappelini"
+    </h1>
+  );
 }
 
 export default Home;
